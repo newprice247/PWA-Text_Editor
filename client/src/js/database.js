a6ec603017e9c2d824f87ae65877127d5461fa59
newprice247 <newprice247@gmail.com>
2023-11-12 11:34:18 -0500

    add database PUT and GET functionality

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUTTING IN DB');
+  const db = await openDB('jate', 1);
+  const transaction = db.transaction('jate', 'readwrite');
+  const store = transaction.objectStore('jate');
+  await store.put({ id: 1, value: content });
+  console.log('PUTTING IN DB DONE');
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GETTING FROM DATABASE');
+  const db = await openDB('jate', 1);
+  const transaction = db.transaction('jate', 'readonly');
+  const store = transaction.objectStore('jate');
+  const content = await store.get(1);
+  const result = await content;
+  console.log('GETTING FROM DATABASE', result.value);
+  return result?.value;
+}
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -39,7 +39,7 @@ module.exports = () => {
         short_name: 'J.A.T.E.',
         description: 'Text Editor',
         background_color: '#225ca3',
-        theme_color: '225ca3',
+        theme_color: '#225ca3',
         start_url: './',
         publicPath: './',
         icons: [
